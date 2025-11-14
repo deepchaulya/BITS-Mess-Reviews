5ddcc856c0cf6e2fdbf689683a55c6d49b306d29
Deep Chaulya <deepchaulya12@gmail.com>
2025-11-15 02:28:06 +0530

    Cleaned test code, ready to implement real project

--- backend/messreviewbackend/src/main/java/com/bits/messreviewbackend/MessreviewbackendApplication.java
+++ backend/messreviewbackend/src/main/java/com/bits/messreviewbackend/MessreviewbackendApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MessreviewbackendApplication {
+public class MessreviewbackendApplication    {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MessreviewbackendApplication.class, args);
