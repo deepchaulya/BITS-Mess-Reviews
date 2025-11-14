e9d5a8e30146510b22ece88f9bed3853b1531147
Deep Chaulya <deepchaulya12@gmail.com>
2025-11-15 00:08:53 +0530

    Added Spring Boot backend skeleton

---
+++ backend/messreviewbackend/src/main/java/com/bits/messreviewbackend/MessreviewbackendApplication.java
@@ -0,0 +1,13 @@
+package com.bits.messreviewbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessreviewbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessreviewbackendApplication.class, args);
+	}
+
+}
---
+++ backend/messreviewbackend/src/test/java/com/bits/messreviewbackend/MessreviewbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bits.messreviewbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessreviewbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
